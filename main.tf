5aa733bb434b2d4094655e86b3bb03842967f456
Arshad Syed <arshad.syed75@gmail.com>
2024-08-05 17:24:40 +0530

    Apply changes generated by AI

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+```
 provider "aws" {
   region = "us-west-2"
 }
@@ -13,7 +14,7 @@ resource "aws_instance" "example" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucket"
-  acl    = "public"
+  acl    = "public-read"
 
   tags = {
     Name        = "example-bucket"
@@ -38,4 +39,10 @@ resource "aws_security_group" "example" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name        = "example-security-group"
+    Environment = "Dev"
+  }
 }
+```
